e68d66a0af6f55a264a02241a8e2e7d38f9b098e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-23 12:59:46 -0800

    Created my_reduce function

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,6 @@
+# to be used with calculator file in calculator-2
+
+
 def add(num1, num2):
     """Return the sum of two numbers"""
     return int(num1) + int(num2)
@@ -20,7 +23,6 @@ def divide(num1, num2):
 
 def square(num):
     """Return the square of a number"""
-
     return int(num[0]) ** 2
 
 
@@ -29,12 +31,20 @@ def cube(num):
     return num ** 3
 
 
-def power(num, exponent):
+def power(num1, num2):
     """Return num raised to the power of exponent"""
-    return int(num) ** int(exponent)
+    return int(num1) ** int(num2)
 
 
 def mod(num1, num2):
     """Return remainder of num1 divided by num2"""
     return int(num1) % int(num2)
-# to be used with calculator file in calculator-2
+
+
+def my_reduce(user_function, user_list):
+    while len(user_list) >= 2:
+        new_first_item = user_function(user_list[0], user_list[1])
+        print new_first_item
+        user_list.pop(0)
+        user_list[0] = new_first_item
+        return user_list[0]
